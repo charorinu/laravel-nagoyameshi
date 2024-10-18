5fcc6f635208b4e66f172d766825633415abe8b5
charorinu <ausuta.gawa1118@gmail.com>
2024-10-18 19:42:37 +0900

    課題 会員側のメール認証機能を有効にしよう会員登録・ログイン後のリダイレクト先を変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/'; // ここを修正
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
