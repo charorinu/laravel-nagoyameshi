b6d3fb410efa899b5f706782f3d2027f19c94264
charorinu <ausuta.gawa1118@gmail.com>
2024-10-22 18:20:08 +0900

    5章 管理者側の認証を実装しよう　管理者用のコントローラを作ろう

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -39,10 +39,10 @@ class LoginRequest extends FormRequest
      */
     public function authenticate(): void
     {
-        $this->ensureIsNotRateLimited();
+        $this->is('admin/*') ? $guard = 'admin' : $guard = 'web';
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-            RateLimiter::hit($this->throttleKey());
+        if (! Auth::guard($guard)->attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+           RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
                 'email' => trans('auth.failed'),
